d0113dde58e2f7a3350796a93d14a1a5b71a3642
henrylopezkre <henrylopezkre@gmail.com>
2018-04-16 01:13:16 -0600

    Se modificó el archivo: main/urls.py

--- Practica3/main/urls.py
+++ Practica3/main/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     re_path(r'^$', views.index, name='index'),
     re_path(r'^iniciar_sesion/', views.iniciar_sesion, name='iniciar_sesion'),
     re_path(r'^cerrar_sesion/', views.cerrar_sesion, name='cerrar_sesion'),
+    re_path(r'^registrar/', views.registrar, name='registrar'),
+    re_path(r'^principal/', views.principal, name='principal'),
 ]

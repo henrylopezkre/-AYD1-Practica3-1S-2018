3053ba78dee558a76027f2dbd2c75d6a80266877
henrylopezkre <henrylopezkre@gmail.com>
2018-04-16 10:15:58 -0600

    Se modificó el archivo: main/models.py

--- Practica3/main/tests.py
+++ Practica3/main/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Usuario, Cuenta, Transaccion, Transferencia, Tipo
 
-# Create your tests here.
+class CuentaTestCase(TestCase):
+	def setUp(self):
+		tipo1 = Tipo.objects.create(descripcion='Préstamo')
+		usuario1 = Usuario.objects.create(nombre='Pedro', apellido='Perez', nickname='pperez', correo='pperez@gmail.com', contrasenia='hola123')
+		usuario2 = Usuario.objects.create(nombre='Maria', apellido='Perez', nickname='mperez', correo='mperez@gmail.com', contrasenia='hola123')
+		cuenta1 = Cuenta.objects.create(codigo=usuario1, cantidad=100.00)
+		cuenta2 = Cuenta.objects.create(codigo=usuario2, cantidad=200.00)
+		Transferencia.objects.create(no_cuenta_origen=cuenta1, no_cuenta_destino=usuario2, monto=50.00)
+		Transaccion.objects.create(monto=75.00, descripcion='Nuevo préstamo', cod_tipo=tipo1)
+
+	def test_cantidad(self):
+		cuenta1 = Cuenta.objects.get(cantidad=100.00)
+		self.assertEqual(cuenta1.cantidad, 100.00)

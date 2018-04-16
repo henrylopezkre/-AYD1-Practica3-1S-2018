6c37d5e2fc6d9938751526b89393eaec9035bfdd
henrylopezkre <henrylopezkre@gmail.com>
2018-04-15 23:44:49 -0600

    Se modificó el archivo: Practica/urls.py

--- Practica3/Practica3/urls.py
+++ Practica3/Practica3/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, re_path, include
 from django.contrib import admin
-from django.urls import path
+from Practica3 import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #re_path(r'^admin/', include(admin.site.urls)),
+    re_path(r'^', include(('main.urls', 'main'), namespace='main')),
 ]

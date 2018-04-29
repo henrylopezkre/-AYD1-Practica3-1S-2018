36866259267495e393c937aee75bc208ca08e50b
henrylopezkre <henrylopezkre@gmail.com>
2018-04-28 18:50:09 -0600

    Se actualizó la última versión

--- Practica3/main/tests/tests_models.py
+++ Practica3/main/tests/tests_models.py
@@ -3,39 +3,65 @@ from django.utils import timezone
 from main.models import Usuario, Cuenta, Tipo, Transferencia, Transaccion
 
 class ModelsTestCase(TestCase):
-
-	def create_usuario(self):
-		return Usuario.objects.create(codigo=1234, nombre='Pedro', apellido='Perez', 
+	def setUp(self):
+		u0 = Usuario.objects.create(cod_usuario=123456, nombre='Pedro', apellido='Perez', 
 			nickname='pperez', correo='pperez@gmail.com', contrasenia='hola123')
+		u1 = Usuario.objects.create(cod_usuario=123457, nombre='Maria', apellido='Castro', 
+			nickname='mcastro', correo='mcastro@gmail.com', contrasenia='hola123')
+		self.c0 = Cuenta.objects.create(no_cuenta=1234567899, cod_usuario=u0, cantidad=1000.00)
+		self.c1 = Cuenta.objects.create(no_cuenta=1234567900, cod_usuario=u1, cantidad=2000.00)
+		self.t1 = Tipo.objects.create(descripcion='Débito')
+		self.t2 = Tipo.objects.create(descripcion='Crédito')
+
 
 	def test_usuario_creation(self):
-		u = self.create_usuario()
-		self.assertTrue(isinstance(u, Usuario))
+	 	u = Usuario.objects.get(cod_usuario=123456)
+	 	self.assertTrue(isinstance(u, Usuario))
 
-	def create_tipo(self):
-		return Tipo.objects.create(descripcion='Prueba')
-
-	def test_tipo_creation(self):
-		u = self.create_tipo()
-		self.assertTrue(isinstance(u, Tipo))
-
-	def create_cuenta(self):
-		return Cuenta.objects.create(no_cuenta=1234567899, codigo=self.create_usuario(), cantidad=1000.00)
-
-	def test_cuenta_creation(self):
-		u = self.create_cuenta()
-		self.assertTrue(isinstance(u, Cuenta))
+	def test_usuario_registered(self):
+	 	u = Usuario.objects.get(cod_usuario=123456)
+	 	self.assertEqual(u.correo, 'pperez@gmail.com')
 
 	def create_transferencia(self):
-		return Transferencia.objects.create(no_cuenta_origen=self.create_cuenta(), no_cuenta_destino=self.create_cuenta(), monto=1000.00)
+		return Transferencia.objects.create(no_cuenta_origen=self.c0, 
+			no_cuenta_destino=self.c1, fecha_hora=timezone.now(), monto=100.00)
 
 	def test_transferencia_creation(self):
-		u = self.create_transferencia()
-		self.assertTrue(isinstance(u, Transferencia))
+	  	tf = self.create_transferencia()
+	  	self.assertTrue(isinstance(tf, Transferencia))
 
-	def create_transaccion(self):
-		return Transaccion.objects.create(no_cuenta=self.create_cuenta(), monto=1000.00, descripcion='Débito', cod_tipo=self.create_tipo())
+	def test_cuenta_destino_exists(self):
+		tf = self.create_transferencia()
+		self.assertTrue(tf.no_cuenta_destino.no_cuenta==1234567900)
 
-	def test_transaccion_creation(self):
-		u = self.create_transaccion()
-		self.assertTrue(isinstance(u, Transaccion))
+	def test_monto_exists(self):
+		tf = self.create_transferencia()
+		self.assertTrue(tf.no_cuenta_origen.cantidad > tf.monto)
+
+	def test_saldo_exists(self):
+		tf = self.create_transferencia()
+		self.assertTrue(tf.no_cuenta_origen.cantidad==1000.00)
+
+	def create_debito(self):
+		return Transaccion.objects.create(no_cuenta=self.c1, fecha_hora=timezone.now(), 
+			monto=500.00, descripcion='Débito', cod_tipo=self.t1)
+
+	def create_credito(self):
+		return Transaccion.objects.create(no_cuenta=self.c1, fecha_hora=timezone.now(), 
+			monto=80.00, descripcion='Crédito', cod_tipo=self.t2)
+
+	def test_debito_destino_exists(self):
+		deb = self.create_debito()
+		self.assertTrue(deb.no_cuenta.no_cuenta==1234567900)
+
+	def test_credito_destino_exists(self):
+		cred = self.create_credito()
+		self.assertTrue(cred.no_cuenta.no_cuenta==1234567900)
+
+	def test_debito_saldo_exists(self):
+		deb = self.create_debito()
+		self.assertTrue(deb.no_cuenta.cantidad > deb.monto)
+
+	def test_credito_saldo_exists(self):
+		cred = self.create_credito()
+		self.assertTrue(cred.no_cuenta.cantidad > cred.monto)
--- Practica3/main/tests/tests_views.py
+++ Practica3/main/tests/tests_views.py
@@ -1,69 +1,23 @@
 from django.test import TestCase, RequestFactory
+from django.urls import reverse
 from django.utils import timezone
-from main.views import obtener_no_cuenta, obtener_codigo, iniciar_sesion, registrar, transferir, debito_credito, saldo, principal, index
+from main.models import Usuario, Cuenta
+from main.views import registrar, obtener_cod
 
 class ViewsTestCase(TestCase):
-	def setUp(self):
+    def setUp(self):
         self.factory = RequestFactory()
-        self.type = Tipo.objects.create(descripcion='Prueba')
-        self.user = Usuario.objects.create(codigo=1234, nombre='Pedro', apellido='Perez', 
+        self.user = Usuario.objects.create(cod_usuario=123456, nombre='Pedro', apellido='Perez', 
 			nickname='pperez', correo='pperez@gmail.com', contrasenia='hola123')
-        self.account =  Cuenta.objects.create(no_cuenta=1234567899, codigo=self.user, cantidad=1000.00)
-        self.transference = Transferencia.objects.create(no_cuenta_origen=self.account,
-        	no_cuenta_destino=self.account, monto=1000.00)
-        self.transaccion = Transaccion.objects.create(no_cuenta=self.account, monto=1000.00, 
-        	descripcion='Débito', cod_tipo=self.type)
-
-    def test_iniciar_sesion(self):
-        request = self.factory.post('main/iniciar_sesion')
-        request.user = self.user
-        response = iniciar_sesion(request)
-        response = iniciar_sesion.as_view()(request)
-        self.assertEqual(response.status_code, 200)
+        self.account =  Cuenta.objects.create(no_cuenta=1234567899, cod_usuario=self.user, cantidad=1000.00)
 
     def test_registrar(self):
         request = self.factory.post('main/registrar')
         request.user = self.user
         response = registrar(request)
-        response = registrar.as_view()(request)
         self.assertEqual(response.status_code, 200)
 
-    def test_transferir(self):
-        request = self.factory.post('main/transferir')
-        request.user = self.user
-        response = transferir(request)
-        response = transferir.as_view()(request)
-        self.assertEqual(response.status_code, 200)
+    def test_registrar_codigo(self):
+        c = obtener_cod(self.user)
+        self.assertEqual(c, 123457)
 
-    def test_debito_credito(self):
-        request = self.factory.post('main/debito_credito')
-        request.user = self.user
-        response = debito_credito(request)
-        response = debito_credito.as_view()(request)
-        self.assertEqual(response.status_code, 200)
-
-    def test_saldo(self):
-        request = self.factory.get('main/saldo')
-        response = saldo(request)
-        response = saldo.as_view()(request)
-        self.assertEqual(response.status_code, 200)
-
-    def test_principal(self):
-        request = self.factory.get('main/principal')
-        response = principal(request)
-        response = principal.as_view()(request)
-        self.assertEqual(response.status_code, 200)
-
-    def test_index(self):
-        request = self.factory.get('main/index')
-        response = index(request)
-        response = index.as_view()(request)
-        self.assertEqual(response.status_code, 200)
-
-	def test_obtener_no_cuenta(self):
-		response = obtener_no_cuenta()
-		self.assertEqual(response, 1234567900)
-
-	def test_obtener_codigo(self):
-		response = obtener_no_cuenta()
-		self.assertEqual(response, 123457)

2c15a2a8aed0fe00c30b20339add53d0e0d3ca3e
henrylopezkre <henrylopezkre@gmail.com>
2018-04-28 21:29:15 -0600

    Se actualizó la última versión

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\henry\documents\analisis y diseño 1\[ayd1]practica3_201314886\practica3_venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
